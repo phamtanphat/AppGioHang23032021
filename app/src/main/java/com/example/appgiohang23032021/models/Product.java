403dc83f6c3232af48d8ced2b5a11121a9be3c17
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-13 21:12:48 +0700

    data mock

--- app/src/main/java/com/example/appgiohang23032021/models/Product.java
+++ app/src/main/java/com/example/appgiohang23032021/models/Product.java
@@ -1,5 +1,11 @@
 package com.example.appgiohang23032021.models;
 
+import com.example.appgiohang23032021.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Product {
     private long id;
     private String name;
@@ -63,5 +69,17 @@ public class Product {
 
     public void setCount(int count) {
         this.count = count;
+    }
+    public static List<Product> getDataMock(){
+        return new ArrayList<>(Arrays.asList(
+                new Product(1,"iPhone 12 Pro Max 256GB", R.drawable.hinhiphone12,37490000,1 , 0),
+                new Product(2,"Xiaomi Mi 11 Lite", R.drawable.hinhxiaomi11,7990000,6 , 0),
+                new Product(3,"Samsung Galaxy Note 20", R.drawable.hinhsamsunggalaxynote20,15990000,4 , 0),
+                new Product(4,"Vivo V21 5G", R.drawable.hinhvivov21,9990000,5 , 0),
+                new Product(5,"Realme 8 Pro", R.drawable.hinhrealme8,8690000,3 , 0),
+                new Product(6,"Xiami Redme 9T", R.drawable.hinhxiaomiredme9t,4290000,3 , 0),
+                new Product(7,"Oppo A74 5G", R.drawable.hinhoppoa74,7490000,2 , 0),
+                new Product(8,"Samsung Galaxy A32", R.drawable.hinhsamsunggalaxya32,6490000,4 , 0)
+        ));
     }
 }
