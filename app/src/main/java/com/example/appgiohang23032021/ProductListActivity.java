a5372de2f5672963a523613415520b3c473d798e
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-05-15 20:38:18 +0700

    gắn menu

--- app/src/main/java/com/example/appgiohang23032021/ProductListActivity.java
+++ app/src/main/java/com/example/appgiohang23032021/ProductListActivity.java
@@ -1,10 +1,13 @@
 package com.example.appgiohang23032021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.SearchManager;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.appgiohang23032021.adapter.ProductAdapter;
 import com.example.appgiohang23032021.models.Product;
@@ -18,6 +21,7 @@ public class ProductListActivity extends AppCompatActivity {
     RecyclerView mRcvProduct;
     ProductAdapter mProductAdapter;
     Toolbar mToolbar;
+    SearchView mSearchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +38,28 @@ public class ProductListActivity extends AppCompatActivity {
 
         mRcvProduct.setHasFixedSize(true);
         mRcvProduct.setAdapter(mProductAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_product,menu);
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        mSearchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        //Hiển thị searchview full chiêu ngang của toolbar
+//        mSearchView.setMaxWidth(Integer.MAX_VALUE);
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+        return true;
     }
 }
